b4f9980e74d1140350cec2bc48ee558331bae196
addisonu <my_wireless_world@yahoo.com>
2015-03-19 19:10:08 -0700

    New concept prep files

---
+++ CPP_Primer/Cyber_Mancing_WBot/Resource_Files/utils/gen_webpage/Updates.cpp
@@ -0,0 +1,25 @@
+// DATE : 03/19/15
+// FILE : Updates.cpp
+// DESC : Updates that apply across a project
+
+// LEFT OFF //
+/*
+ */
+
+// BUGS //
+/*
+ */
+
+// POSSIBLE SOLUTIONS/ADDITIONS //
+/*
+ * Add comments to all files
+ * Add preconditions and postconditions to all functions
+ * Start writing unit tests using Unit_test class
+ * Integrate data structures into find_url and resolve_url programs
+ * Start keeping log of actions in a draft design log in preparation for project
+ */
+
+// NOTES //
+/*
+ */
+
