e5fa8699d4d2da2e20f2eaa1b87aad62a2fe8279
addisonu <my_wireless_world@yahoo.com>
2015-04-08 15:23:05 -0700

    Fleshed out unit tests for Indexer_test.cpp

--- CPP_Primer/Cyber_Mancing_WBot/Resource_Files/utils/unit_tests/Indexer_test.cpp
+++ CPP_Primer/Cyber_Mancing_WBot/Resource_Files/utils/unit_tests/Indexer_test.cpp
@@ -27,7 +27,7 @@
 
 // Class header files
 
-#include "Indexer.h"
+#include "../gen_webpage/Indexer.h"
 
 using namespace CppUnit;
 
@@ -35,11 +35,11 @@ class Indexer_test : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(Indexer_test);
     CPPUNIT_TEST(set_word_test);
-    CPPUNIT_TEST(operator[]_test);
+    CPPUNIT_TEST(op_sub_test);
     CPPUNIT_TEST(find_word_test);
     CPPUNIT_TEST(add_test);
     CPPUNIT_TEST(gen_webpage_test);
-    CPPUNIT_TEST(operator<<_test);
+    CPPUNIT_TEST(op_out_test);
     CPPUNIT_TEST_SUITE_END();
 
 public :
@@ -47,56 +47,99 @@ public :
     void tearDown();
 
 protected :
-    set_word_test();
-    void operator[]_test();
+    void set_word_test();
+    void op_sub_test();
     void find_word_test();
     void add_test();
     void gen_webpage_test();
-    void operator<<_test();
+    void op_out_test();
 
 private :
     Indexer idx_obj;
+    Word non;
+    Word w1, w2;
 };
 
 //===========================================================
 
 void Indexer_test::setUp()
 {
-
+    w1.set_word("setUp");
+    w1.set_count(1);
+    w1.add_url("http://www.setUp.com");
+    idx_obj.add(w1);
 }
 
 void Indexer_test::tearDown()
 {
-
+    ;
 }
+
 void Indexer_test::set_word_test()
 {
-
+    w2.set_word("set_word");
+    idx_obj.set_word(w1.get_word(), w2);
+    CPPUNIT_ASSERT(idx_obj.find_word(w2.get_word(), non).get_word() == "set_word");
+    CPPUNIT_ASSERT(idx_obj.find_word(w2.get_word(), non).get_count() == w1.get_count());
+    CPPUNIT_ASSERT(idx_obj.find_word(w2.get_word(), non).get_url() == w1.get_url());
 }
 
-void Indexer_test::operator[]_test()
+void Indexer_test::op_sub_test()
 {
-
+    Word w3("op_sub3", 3, "http://www.u1.com http://www.u2.com http://www.u3.com");
+    idx_obj.add(w3);
+    CPPUNIT_ASSERT(idx_obj[w3.get_word()].get_word() == "op_sub3");
+    CPPUNIT_ASSERT(idx_obj[w3.get_word()].get_count() == 3);
+    CPPUNIT_ASSERT(idx_obj[w3.get_word()].get_url() == "http://www.u1.com\nhttp://www.u2.com\nhttp://www.u3.com\n");
 }
 
 void Indexer_test::find_word_test()
 {
-
+    std::string str("find_word");
+    w1.set_word(str);
+    idx_obj.set_word(w2.get_word(), w1);
+    CPPUNIT_ASSERT(idx_obj.find_word(str, non).get_word() == str);
 }
 
 void Indexer_test::add_test()
 {
+    Word u0, u1("u1", 1, "http://www.u1.com");
 
+    idx_obj.add(u0);
+    CPPUNIT_ASSERT(idx_obj.find_word(u0.get_word(), non).get_word() == "");
+
+    idx_obj.add(u1);
+    CPPUNIT_ASSERT(idx_obj.find_word(u1.get_word(), non).get_word() == "u1");
 }
 
 void Indexer_test::gen_webpage_test()
 {
+    Indexer idx_obj1;
+    Word u0("gen_webpage0", 1, "http://www.gen_webpage0.com");
+    Word u1("gen_webpage1", 2, "http://www.gen.com http://www.webpage.com");
 
+    idx_obj1.add(u0);
+    idx_obj1.add(u1);
+
+    idx_obj1.gen_webpage();
+    std::ifstream txt("webpages_idx/");
+    std::string str1, str2;
+    unsigned cnt(0);
+
+    while(std::getline(txt, str1)){
+        str2 += str1;
+    }
+
+    CPPUNIT_ASSERT(str2.find("gen_webpage0") != std::string::npos);
+    CPPUNIT_ASSERT(str2.find("http://www.gen_webpage0.com") != std::string::npos);
+    CPPUNIT_ASSERT(str2.find("gen_webpage1") != std::string::npos);
+    CPPUNIT_ASSERT(str2.find("http://www.gen.com") != std::string::npos);
+    CPPUNIT_ASSERT(str2.find("http://www.webpage.com") != std::string::npos);
 }
 
-void Indexer_test::operator<<_test()
+void Indexer_test::op_out_test()
 {
-
+    ;
 }
 
 //===========================================================
