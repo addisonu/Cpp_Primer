3137a80e23f11e7f4c5e8ce4adebf332a1cecee5
addisonu <my_wireless_world@yahoo.com>
2015-04-15 17:50:45 -0700

    Added output code for rank_song.cpp and wrote alternate program to produce same output

--- CPP_Primer/ExCl/rank_songs_lam.cpp
+++ CPP_Primer/ExCl/rank_songs_lam.cpp
@@ -1,14 +1,15 @@
-// Rank songs using Zip's law and print the m most played of the n album songs
+// Rank songs using Zip's law and print the m most played of the n album songs. Alternate implementaion using a lambda to sort songs in album.
 
 #include <iostream>
 #include <vector>
-#include <list>
 #include <sstream>
 #include <string>
+#include <algorithm>
 #include <utility>
 
 int main(int argc, char **argv)
 {
+// Get number of songs in album and number of most played songs to display
     unsigned song_num(0), top_num(0);
     std::string input;
     std::cout << "Enter the number of songs on the album.\n";
@@ -19,11 +20,11 @@ int main(int argc, char **argv)
     std::getline(std::cin, input);
     top_num = atoi(input.c_str());
 
+// Each element of album holds number of plays and song name
     std::vector<std::pair<double, std::string>> album;
-    std::list<std::pair<double, std::string>> hits;
-
     std::pair<double, std::string> song;
 
+// Get each album elements from user
     for(unsigned i = song_num; i > 0; --i){
         std::cout << "Enter the next song.\n";
         std::getline(std::cin, input);
@@ -33,30 +34,14 @@ int main(int argc, char **argv)
         ss >> song.second;
         album.push_back(song);
     }
-
-// Check if new song is one of the top plays
-    if(top_num){
-        std::pair<double, std::string> max(0, ""), last(0, "");
-        for(int i = 0; i != top_num; ++i){
-            for(auto ele : album){
-                if(i == 0){
-                    if(ele.first > max.first)
-                        max = ele;
-                }
-                else{
-                    if(ele.first < last.first && ele.first > max.first){
-                       max = ele;
-                    }
-                }
-            }
-                hits.push_back(max);
-                last = max;
-                max.first = 0;
-        }
+// Sort songs using library sort function and lambda
+    if(top_num && album.size()){
+        std::sort(album.begin(), album.end(), [ ](std::pair<double, std::string> p1, std::pair<double, std::string> p2) -> bool { return p1.first > p2.first; });
     }
+
 // Print most played songs
-    for(auto song : hits)
-        std::cout << song.second << std::endl;
+    for(auto i = album.begin(); i != album.begin() + top_num; ++i)
+        std::cout << i->second << std::endl;
 
     return 0;
 }
