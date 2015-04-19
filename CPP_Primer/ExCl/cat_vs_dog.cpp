e6831ab94453602fddb0c56eaa9edf6761deb938
addisonu <my_wireless_world@yahoo.com>
2015-04-18 22:29:52 -0700

    new exercise

---
+++ CPP_Primer/ExCl/cat_vs_dog.cpp
@@ -0,0 +1,26 @@
+// DATE : 04/18/15
+// FILE : cat_vs_dog.cpp
+// DESC : Finds the maximum number of satisfied voters for each testcase
+// INPUT //
+// n testcases, n < = 100
+// per testcase:
+// c, d, v (1 ≤ c, d ≤ 100 and 0 ≤ v ≤ 500): the number of cats, dogs, and voters.
+// v lines with two pet identifiers : C# || D#, wher the firt indentifier is the pet to keep and second is pet release
+// OUTPUT //
+// Per testcase: One line with the maximum possible number of satisfied voters for the show.
+
+// LEFT OFF //
+/*
+ */
+
+// BUGS //
+/*
+ */
+
+// POSSIBLE SOLUTIONS/ADDITIONS //
+/*
+ */
+
+// NOTES //
+/*
+ */
