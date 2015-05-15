c2e88736efc841f97185a47e42082e2a2369623c
addisonu <my_wireless_world@yahoo.com>
2015-05-14 23:03:49 -0700

    Add functionality to Cyber_Mancing_WBot

---
+++ CPP_Primer/Cyber_Mancing_WBot/Resource_Files/utils/robot_filter/Robot_filter.cpp
@@ -0,0 +1,36 @@
+// DATE : 05/14/15
+// FILE : Robot_filter.cpp
+// DESC : Class to read robot.txt files and identify URLs that should be excluded from index
+
+// LEFT OFF //
+/*
+ */
+
+// BUGS //
+/*
+ */
+
+// POSSIBLE SOLUTIONS/ADDITIONS //
+/*
+ */
+
+// NOTES //
+/*
+ */
+
+class Robot_filter {
+public :
+
+// CONSTRUCTORS //
+Robot_filter() {}
+
+// MEMBER FUNCTIONS //
+
+// PRECONDITIONS :
+// POSTCONDITIONS :
+bool get_excluded(  istream & input) {  }
+
+// PRECONDITIONS :
+// POSTCONDITIONS :
+bool is_exclueded(const char *url) {  }
+};
