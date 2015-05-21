6853ed89d9fcc868c27e2e8934e305c1fcbdbaec
addisonu <my_wireless_world@yahoo.com>
2015-05-20 19:55:42 -0700

    APAN : variadic template

--- CPP_Primer/A_Program_A_Night/APAN:05.19.15.cpp
+++ CPP_Primer/A_Program_A_Night/APAN:05.19.15.cpp
@@ -1,3 +1,47 @@
 // DATE : 05/19/15
 // FILE : APAN:05.19.15.cpp
 // DESC : Variable length parameter list
+
+#include <iostream>
+#include <string>
+
+// Forward declarations
+template <typename T, typename... args> double avg(const T &num, const args&... nums);
+template <typename T, typename... args> double sum(const T &num, const args&... nums);
+template <typename T> double sum(const T &num);
+template <class T, class...args> std::ostream& print(std::ostream &out, const T &obj, const args&... objs);
+template <class T> std::ostream& print(std::ostream &out, const T &obj);
+
+int main(){
+
+// Invoke avg
+    std::cout << avg(1, 2, 3, 4, 5, 6, 7) << std::endl;
+
+// Invoke print
+    print(std::cout, "first", 2, 3.14, 'c', -8, "last");
+    return 0;
+}
+
+template <typename T, typename... args> double avg(const T &num, const args&... nums){
+    return sum(num, nums...) / (sizeof...(args) + 1);
+}
+template <typename T, typename... args> double sum(const T &num, const args&... nums)
+{
+    return num + sum(nums...);
+}
+
+template <typename T> double sum(const T &num)
+{
+    return num;
+}
+
+template <class T, class...args> std::ostream& print(std::ostream &out, const T &obj, const args&... objs)
+{
+    out << obj << ", size : " << sizeof(obj) << std::endl;
+    return print(out, objs...);
+}
+
+template <class T> std::ostream& print(std::ostream &out, const T &obj)
+{
+    return out << obj << ", size : " << sizeof(obj) << std::endl;
+}
