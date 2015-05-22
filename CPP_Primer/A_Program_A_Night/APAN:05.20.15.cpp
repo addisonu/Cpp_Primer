e618d8e46b7f7f1fdb975b34616594cce15c4014
addisonu <my_wireless_world@yahoo.com>
2015-05-21 21:44:56 -0700

    APAN : template specialization

--- CPP_Primer/A_Program_A_Night/APAN:05.20.15.cpp
+++ CPP_Primer/A_Program_A_Night/APAN:05.20.15.cpp
@@ -9,30 +9,56 @@
 // Forward declarations
 template <typename T> class Tclass;
 template <typename T> std::ostream& operator<<(std::ostream &out, const Tclass<T> &obj);
-template <typename T> std::ostream& operator<<(std::ostream &out, const int &obj);
-
 
 template <typename T> class Tclass{
+
+public :
+
 // TYPE MEMBERS //
+    using type = T;
 
 // MEMBER FUNCTIONS //
-    Tclass(T val) { a = val; }
+    Tclass(type val) { a = val; }
+    void set(type val) { a = val; }
+    type get() { return a;}
+
+private :
 
 // DATA MEMBERS //
-    private double a = 1.23;
+    type a;
 
 // FRIENDS //
-    friend std::ostream& operator<<<T>(std::ostream &out, const Tclass<T> &obj);
+    friend std::ostream& operator<<<type>(std::ostream &out, const Tclass<type> &obj);
 };
 
-template <> std::ostream& operator<<(std::ostream &out, const int &obj)
+template <typename T> std::ostream& operator<<(std::ostream &out, const Tclass<T> &obj)
+{
+    out << obj.a;
+    return out;
+}
+
+
+template <> std::ostream& operator<<(std::ostream &out, const Tclass<int> &obj)
 {
     out << obj.a * obj.a;
+    return out;
 }
+
+template <> void Tclass<int>::set(int val) { a = val * val; }
+
+template <> int Tclass<int>::get() { return a * a; }
 
 int main()
 {
-    Tclass t(7);
-    std::cout << t << std::endl;
+    Tclass<int> t0(7);
+    std::cout << t0 << std::endl;
+    t0.set(8);
+    std::cout << t0.get() << std::endl;
+
+    Tclass<std::string> t1("string arg");
+    std::cout << t1 << std::endl;
+    t1.set("another string arg");
+    std::cout << t1.get() << std::endl;
+
     return 0;
 }
