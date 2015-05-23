1fd8b496e9178e1778fd514467534d1f772a555f
addisonu <my_wireless_world@yahoo.com>
2015-05-22 21:40:18 -0700

    APAN : multidimensional array

---
+++ CPP_Primer/A_Program_A_Night/APAN:05.22.15.cpp
@@ -0,0 +1,47 @@
+// DATE : 05/22/15
+// FILE : APAN:05.22.15.cpp
+// DESC : Use multidimensional array
+
+#include <iostream>
+
+int main()
+{
+// Define multidimensional array
+    double darr0[2][2][2]{{{0}}};
+    double darr1 [2][2][2] = {0};
+    double darr2 [2][2][1] = {1,1,1,1};
+
+// Initialize
+    constexpr std::size_t dim1 = 3, dim2 = 3, dim3 = 2;
+    int iarr[dim1][dim2][dim3]= {{{1,2},{3,4},{5,6}},{{7,8},{9,10},{11,12}},{{13,14},{15,16},{17,18}}};
+
+// Subscript
+    std::cout << "darr0[0][1][1] = " << darr0[0][1][1] << std::endl;
+    std::cout << "*darr2[1][0] = " << *darr2[1][0] << std::endl;
+    std::cout << "iarr : " << std::endl;
+    for(std::size_t i = 0; i != dim1; ++i)
+        for(std::size_t j = 0; j != dim2; ++j)
+            for(std::size_t k = 0; k != dim3; ++k)
+                std::cout << iarr[i][j][k] << std::endl;
+// Assign
+    darr1[0][1][0] = 2;
+    std::cout << "darr1[0][1][0] = " << darr1[0][1][0] << std::endl;
+
+// Iterating with range for
+    std::cout << "iarr : " << std::endl;
+    for(auto &ele0 : iarr)
+        for(auto &ele1 : ele0)
+            for(auto ele2 : ele1)
+                std::cout << ele2 << " ";
+    std::cout << std::endl;
+
+// Defining pointer
+    using pi = int (*)[dim3];
+    pi ele = &iarr[2][1];
+    std::cout << "*ele[1] = " << ele[1][1] << std::endl;
+
+// array name conversion to pointer
+    std::cout << "iarr = " << iarr << std::endl;
+
+    return 0;
+}
