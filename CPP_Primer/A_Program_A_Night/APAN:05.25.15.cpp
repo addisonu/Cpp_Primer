6a36d5cafca812129a95fbf4c457d7d618466e6e
addisonu <my_wireless_world@yahoo.com>
2015-05-25 21:26:47 -0700

    APAN : command-line arguments

--- CPP_Primer/A_Program_A_Night/APAN:05.25.15.cpp
+++ CPP_Primer/A_Program_A_Night/APAN:05.25.15.cpp
@@ -1,3 +1,26 @@
 // DATE : 05/25/15
 // FILE : APAN:05.25.15.cpp
-// DESC : 
+// DESC : Print arguments passed via command-line
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+// OUTPUT
+/*
+    argv[1] : Happy
+    argv[2] : Memorial
+    argv[3] : Day
+ */
+
+    std::size_t cnt(1);
+
+    for(auto pa0 = argv + 1; pa0 != argv + argc; ++pa0){
+        std::cout << "argv[" << cnt++ << "] : ";
+        for(auto pa1 = *pa0; pa1 != *pa0 + strlen(*pa0); ++pa1)
+            std::cout << *pa1;
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
