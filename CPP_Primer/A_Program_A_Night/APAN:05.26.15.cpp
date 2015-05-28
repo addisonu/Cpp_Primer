76d348f069846f1bf3203253bf24d1ceb402cab8
addisonu <my_wireless_world@yahoo.com>
2015-05-28 10:35:40 -0600

    APAN : lambda

--- CPP_Primer/A_Program_A_Night/APAN:05.26.15.cpp
+++ CPP_Primer/A_Program_A_Night/APAN:05.26.15.cpp
@@ -1,3 +1,41 @@
-// DATE : 
-// FILE : 
-// DESC : 
+// DATE : 05/26/15
+// FILE : APAN:05.26.15.cpp
+// DESC : Pass lambda as predicate to library algorithm
+
+#include <iostream>
+#include <numeric>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+unsigned total(std::string str);
+
+int main()
+{
+// Container objects to operator on
+    std::vector<std::string> svec = {"zzzing0", "str1", "String2s", "ring3s"};
+    std::vector<double> dvec{1.1,2.2,3.3,4.4};
+    std::string str("Input string");
+
+// stable sort will sort strings in ascending order according to the sum of the decimal values of each char
+    stable_sort(svec.begin(), svec.end(), [ ] (std::string str_arg1,  std::string str_arg2) -> bool { return total(str_arg1) < total(str_arg2); });
+    for(auto val : svec)
+        std::cout << val << std::endl;
+
+// find_if returns strings ending in 's'
+    auto found = find_if(svec.cbegin(), svec.cend(), [] (std::string str_arg) -> bool { return *(--str_arg.cend()) == 's'; });
+    std::cout << "found = ";
+    for(auto val : *found)
+         std::cout << val;
+    std::cout << std::endl;
+
+    return 0;
+}
+
+unsigned total(std::string str)
+{
+    unsigned sum(0);
+    for(auto ele : str)
+        sum += ele;
+    return sum;
+}
