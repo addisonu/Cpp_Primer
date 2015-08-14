a41d2eb6a61fa2abc5bbac7f559acaa720017eb0
addisonu <my_wireless_world@yahoo.com>
2015-08-14 13:38:22 -0400

    APAN : primer output

---
+++ CPP_Primer/A_Program_A_Night/APAN:08.04.15.cpp
@@ -0,0 +1,36 @@
+// DATE : 08/04/15
+// FILE : APAN:08.04.15.cpp
+// DESC : C++ Primer exercises : 1.1, 1.3, 1.4, 1.5
+
+#include <iostream>
+
+int main()
+{
+/*
+//Ex 1.1
+   return -1; // returns error 255
+
+// Ex 1.3
+    std::cout << "Hello, World" << std::endl;
+
+// Ex 1.4
+    int v1(0), v2(0);
+    std::cout << "Enter two integers.\n";
+    std::cin >> v1 >> v2;
+
+    std::cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+
+// Ex 1.5
+    int v1(0), v2(0);
+    std::cout << "Enter two integers.\n";
+    std::cin >> v1 >> v2;
+
+    std::cout << "The product of ";
+    std::cout << v1;
+    std::cout << " and ";
+    std::cout << v2;
+    std::cout << " is ";
+    std::cout << v1 * v2;
+    std::cout << std::endl;
+*/    return 0;
+}
