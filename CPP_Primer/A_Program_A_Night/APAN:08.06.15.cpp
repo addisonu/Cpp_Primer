5baa996f7a91a9a4753ff920dec87b59cc615fd6
addisonu <my_wireless_world@yahoo.com>
2015-08-14 14:20:27 -0400

    APAN : primer loop

---
+++ CPP_Primer/A_Program_A_Night/APAN:08.06.15.cpp
@@ -0,0 +1,41 @@
+// DATE : 08/06/15
+// FILE : APAN:08.06.15.cpp
+// DESC : C++ Primer exercises : 1.11, 1.13, 1.15, 1.16
+
+#include <iostream>
+
+int main()
+{
+/*
+// Ex 1.11
+    std::cout << "Enter two integers.\n";
+    int v1(0), v2(0);
+    std::cin >> v1 >> v2;
+
+    for(auto i = v1; i <= v2; ++i)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+// Ex 1.13
+    int sum(0);
+    for(int val = 1; val <= 10; ++val)
+        sum += val;
+    std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+
+// Ex 1.15
+    std::cout < "error"; // syntax errror
+    int val("error"); // type error
+    val = val2; // declaration error
+
+// Ex 1.16
+    int val(0);
+    if(std::cin >> val){
+        int total = val;
+        while(std::cin >> val){
+            total += val;
+        }
+        std::cout << "The sum of input is " << total << std::endl;
+    }
+*/
+    return 0;
+}
