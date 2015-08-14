af477e85f72287cc4dcb12d1f26233069b3fb386
addisonu <my_wireless_world@yahoo.com>
2015-08-14 14:02:19 -0400

    APAN : primer comments

--- CPP_Primer/A_Program_A_Night/APAN:08.05.15.cpp
+++ CPP_Primer/A_Program_A_Night/APAN:08.05.15.cpp
@@ -6,12 +6,31 @@
 
 int main()
 {
-//Ex 1.7
+// Ex 1.7 code changed to allow compiling
 /* 
- * comment pair /* */ cannot nest.
+ * comment pair ** cannot nest.
  * "cannot nest" is considered source code,
  * as is the rest of the program
- */
+ */ 
 
+// Ex 1.8 code changed to allow compiling
+
+//    std::cout << "/*";
+//    std::cout << "*/";
+//    std::cout << /* "*//*" */"";
+//    std::cout << /* "*/" /* "/*" */;
+
+/*
+// Ex 1.9
+    int val(49), sum(0);
+    while(++val <= 100)
+        sum += val;
+    std::cout << "The sum is " << sum << std::endl;
+// Ex 1.10
+    int val(11);
+    while(--val >= 0)
+        std::cout << val << " ";
+    std::cout << std::endl;
+*/
     return 0;
 }
