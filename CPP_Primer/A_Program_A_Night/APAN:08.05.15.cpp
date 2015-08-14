f254b0e2e118bc7eb3ebbb1f87e4c55e4bbb4d59
addisonu <my_wireless_world@yahoo.com>
2015-08-14 14:01:26 -0400

    APAN : primer comments

---
+++ CPP_Primer/A_Program_A_Night/APAN:08.05.15.cpp
@@ -0,0 +1,17 @@
+// DATE : 08/05/15
+// FILE : APAN:08.05.15.cpp
+// DESC : C++ Primer exercises : 
+
+#include <iostream>
+
+int main()
+{
+//Ex 1.7
+/* 
+ * comment pair /* */ cannot nest.
+ * "cannot nest" is considered source code,
+ * as is the rest of the program
+ */
+
+    return 0;
+}
