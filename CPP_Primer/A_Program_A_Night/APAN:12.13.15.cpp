f5e2b982b2a4e84d1484c183032b318ea45a1140
addisonu <my_wireless_world@yahoo.com>
2015-12-13 05:25:19 -0500

    Greting to urs program

---
+++ CPP_Primer/A_Program_A_Night/APAN:12.13.15.cpp
@@ -0,0 +1,11 @@
+// DATE : 12/13/15
+// FILE : APAN:12.13.15.cpp
+// DESC : A hello urs program
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Ohayou gozaimasu urs! (x_x)" << std::endl;
+    return 0;
+}
