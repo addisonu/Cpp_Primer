7b9cf95c7cf3dc932e39de71dd45548c2df32b62
addisonu <my_wireless_world@yahoo.com>
2017-09-21 12:35:43 -0400

    Start Node implementation file

---
+++ CPP_Primer/AI_F17/8_Puzzle/Search_Tree/Node.cpp
@@ -0,0 +1,44 @@
+//AUTHOR: NA
+//DATE: 09/19/17
+//FILE: Node.cpp
+//DESC: Implement all 8-puzzle search elements
+
+#include <queue>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <climits>
+#include "Node.h"
+
+double EightPuzzle::search_time()
+{
+
+    return 0;
+}
+
+unsigned EightPuzzle::manhattan_heuristic(const Node &node)
+{
+
+    return 0;
+}
+
+unsigned EightPuzzle::misplaced_tile_heuristic(const Node &node)
+{
+
+    return 0;
+}
+
+void EightPuzzle::a_star_search(Node &result)
+{
+
+}
+
+void EightPuzzle::ida_search(Node &result)
+{
+
+}
+
+void EightPuzzle::df_branch_bound_search(Node &result)
+{
+
+}
