0fe3e63d347b55bf36ec5fa082b2bfd3f86b2dc4
addisonu <my_wireless_world@yahoo.com>
2017-09-23 16:18:23 -0400

    Fix errors in move and heuristic functions

--- CPP_Primer/AI_F17/8_Puzzle/Search_Tree/Node.cpp
+++ CPP_Primer/AI_F17/8_Puzzle/Search_Tree/Node.cpp
@@ -10,86 +10,94 @@
 #include <climits>
 #include <exception>
 #include <algorithm>
+#include <cmath>
 #include "Node.h"
 
-Node move(const &Node node, MOVE move/*const std::string &move*/)
+Node EightPuzzle::move(/*const*/ Node &node, MOVE move/*const std::string &move*/)
 {
-    //const std::string &move;
+    // create new node to hold new state
     Node new_node;
     auto state = node.state;
+
+    // get position of blank tile to move in puzzle
     auto blank_pos = state.find('b');
 
     // check that move is possible and determine new state
-    if(move == MOVE::LEFT && (pos != 0 && pos != 3 && pos != 6)){
-        auto neighbor = state[pos - 1];
-        state[pos - 1] = 'b';
-        state[pos] = neighbor;
+    if(move == MOVE::LEFT && (blank_pos != 0 && blank_pos != 3 && blank_pos != 6)){
+        auto neighbor_tile = state[blank_pos - 1];
+        state[blank_pos - 1] = 'b';
+        state[blank_pos] = neighbor_tile;
 
         try{
-            // check if node is sibling or child of current node
-            get_tree()->get_explored_set()->at(state);
-            new_node.parent = node.parent;// fix to avoid creating new node
+            // check if new state is reversal of previous move
+            tree.get_explored_set()->at(state);
+            // node has already been generated, fall through to return statement
+            //new_node.parent = node.parent;// debugging: fix to avoid creating new node
         }
         catch(const std::out_of_range& oor){
+            // new_node hasn't been generated before, update new node
             new_node.parent = &node;
+            new_node.state = state;
+            new_node.action = "left";
+            new_node.path_cost = node.path_cost;
         }
-        // update new node
-        new_node.state = state;
-        new_node.action = "left";
     }
-    else if(move == MOVE::UP && (pos != 0 && pos != 1 && pos != 2)){
-        auto neighbor = state[pos - 3];
-        state[pos - 3] = 'b';
-        state[pos] = neighbor;
+    else if(move == MOVE::UP && (blank_pos != 0 && blank_pos != 1 && blank_pos != 2)){
+        auto neighbor_tile = state[blank_pos - 3];
+        state[blank_pos - 3] = 'b';
+        state[blank_pos] = neighbor_tile;
 
         try{
-            // check if node is sibling or child of current node
-            get_tree()->get_explored_set()->at(state);
-            new_node.parent = node.parent;// fix to avoid creating new node
+            // check if new state is reversal of previous move
+            tree.get_explored_set()->at(state);
+            // node has already been generated, fall through to return statement
+            //new_node.parent = node.parent;// debugging: fix to avoid creating new node
         }
         catch(const std::out_of_range& oor){
+            // new_node hasn't been generated before, update new node
             new_node.parent = &node;
+            new_node.state = state;
+            new_node.action = "up";
+            new_node.path_cost = node.path_cost;
         }
-        // update new node
-        new_node.state = state;
-        new_node.action = "up";
     }
-    else if(move == MOVE::RIGHT && (pos != 2 && pos != 5 && pos !- 8)){
-        auto neighbor = state[pos + 1];
-        state[pos + 1] = 'b';
-        state[pos] = neighbor;
+    else if(move == MOVE::RIGHT && (blank_pos != 2 && blank_pos != 5 && blank_pos != 8)){
+        auto neighbor_tile = state[blank_pos + 1];
+        state[blank_pos + 1] = 'b';
+        state[blank_pos] = neighbor_tile;
 
         try{
-            // check if node is sibling or child of current node
-            get_tree()->get_explored_set()->at(state);
-            new_node.parent = node.parent;// fix to avoid creating new node
+            // check if new state is reversal of previous move
+            tree.get_explored_set()->at(state);
+            // node has already been generated, fall through to return statement
+            //new_node.parent = node.parent;// debugging: fix to avoid creating new node
         }
         catch(const std::out_of_range& oor){
+            // new_node hasn't been generated before, update new node
             new_node.parent = &node;
+            new_node.state = state;
+            new_node.action = "right";
+            new_node.path_cost = node.path_cost;
         }
-        // update new node
-        new_node.state = state;
-        new_node.action = "right";
     }
-    else if(move == MOVE::DOWN && (pos != 6 && pos != 7 && pos != 8)){
-        auto neighbor = state[pos + 3];
-        state[pos + 3] = 'b';
-        state[pos] = neighbor;
+    else if(move == MOVE::DOWN && (blank_pos != 6 && blank_pos != 7 && blank_pos != 8)){
+        auto neighbor_tile = state[blank_pos + 3];
+        state[blank_pos + 3] = 'b';
+        state[blank_pos] = neighbor_tile;
 
         try{
-            // check if node is sibling or child of current node
-            get_tree()->get_explored_set()->at(state);
-            new_node.parent = node.parent;// fix to avoid creating new node
+            // check if new state is reversal of previous move
+            tree.get_explored_set()->at(state);
+            // node has already been generated, fall through to return statement
+            //new_node.parent = node.parent;// debugging: fix to avoid creating new node
         }
         catch(const std::out_of_range& oor){
+            // new_node hasn't been generated before, update new node
             new_node.parent = &node;
+            new_node.state = state;
+            new_node.action = "down";
+            new_node.path_cost = node.path_cost;
         }
-        // update new node
-        new_node.state = state;
-        new_node.action = "down";
-    }
-    else{
-        return node;
     }
     return node;
 }
@@ -107,16 +115,25 @@ double EightPuzzle::search_time()
 */
 unsigned EightPuzzle::manhattan_heuristic(const Node &node)
 {
-    // the sum of the distances of the tiles from their goal positions. Because tiles
-    // cannot move along diagonals, the distance we will count is the sum of the horizontal
-    // and vertical distances
+    // the sum of the horizontal and vertical distances of the tiles from their goal positions
+    // will return the h(n) portion of f(n)
     unsigned ver_dis(0);
     unsigned hor_dis(0);
 
-    for(std::size_t i = 0; i != goal_state.size(); ++i){
-        if(goal_state[i] != node.state[i]){
-            ver_dis += pos / 3;
-            hor_dis += pos % 3;
+    // pos / 3 will give row, pos % 3 will give col
+    for(std::size_t pos = 0; pos != goal_state.size(); ++pos){
+        if(goal_state[pos] != node.state[pos]){
+            // find col and row of goal state
+            auto goal_row = pos / 3;
+            auto goal_col = pos % 3;
+
+            // find col and row of node state
+            std::size_t node_pos = node.state.find(goal_state[pos]);
+            auto node_row = node_pos / 3;
+            auto node_col = node_pos % 3;
+
+            ver_dis += std::abs(static_cast<double>(goal_row - node_row));
+            hor_dis += std::abs(static_cast<double>(goal_col - node_col));
         }
     }
     return ver_dis + hor_dis;
@@ -134,9 +151,10 @@ unsigned EightPuzzle::misplaced_tile_heuristic(const Node &node)
     return misplaced_tile;
 }
 
-void EightPuzzle::a_star_search(std::string initial_state, Node &result)
+// NOTE: need to generate real g(n) not use from heuristic
+void EightPuzzle::a_star_search(const std::string &initial_state, Node &result)
 {
-    // add roo to frontier_set
+    /*// add roo to frontier_set
     Node root;
     root.state = initial_state;
     tree.get_frontier_set()->push(root);
@@ -144,6 +162,9 @@ void EightPuzzle::a_star_search(std::string initial_state, Node &result)
     // get top node and start searching for goal
     Node *current = nullptr;
     while(!tree.get_frontier_set().empty() && (current = tree.get_frontier_set()->top().state)) && goal_test(goal_state, current.state){
+        if(current.state == initial_state){
+            continue; // don't process root node
+        }
         //Node current_node = tree.get_frontier_set()->top();
         tree.get_frontier_set()->pop();
 
@@ -183,14 +204,16 @@ void EightPuzzle::a_star_search(std::string initial_state, Node &result)
         explored_set[current.state] = current;
     }
     result = tree.get_frontier_set()->top();
+*/
 }
 
-void EightPuzzle::ida_search(Node &result)
+void EightPuzzle::ida_search(const std::string &initial_state, Node &result)
 {
-
+    Node root;
+    //root.state;
 }
 
-void EightPuzzle::df_branch_bound_search(Node &result)
+void EightPuzzle::df_branch_bound_search(const std::string &initial_state, Node &result)
 {
 
 }
