6c8ef070f7ce5db2aa8c9d368d028f0a2a8bf704
addisonu <my_wireless_world@yahoo.com>
2017-09-27 13:42:19 -0400

    Remove some commented code

--- CPP_Primer/AI_F17/8_Puzzle/Search_Tree/Node.cpp
+++ CPP_Primer/AI_F17/8_Puzzle/Search_Tree/Node.cpp
@@ -5,16 +5,19 @@
 
 #include <queue>
 #include <vector>
+#include <set>
 #include <unordered_map>
 #include <string>
 #include <climits>
 #include <exception>
-#include <algorithm>
+//#include <algorithm>
+#include <iostream> // debug
+#include <stdlib.h> // debug
 #include <cmath>
-#include <iterator>
 #include "Node.h"
 
-Node EightPuzzle::move(/*const*/ Node &node, MOVE move/*const std::string &move*/)
+// update to take different heuristics
+Node EightPuzzle::move(Node &node, MOVE move)
 {
     // create new node to hold new state
     Node new_node;
@@ -29,84 +32,98 @@ Node EightPuzzle::move(/*const*/ Node &node, MOVE move/*const std::string &move*
         state[blank_pos - 1] = 'b';
         state[blank_pos] = neighbor_tile;
 
-        try{
-            // check if new state is reversal of previous move
-            tree.get_explored_set()->at(state);
-            // node has already been generated, fall through to return statement
-            //new_node.parent = node.parent;// debugging: fix to avoid creating new node
-        }
-        catch(const std::out_of_range& oor){
-            // new_node hasn't been generated before, update new node
-            new_node.parent = &node;
-            new_node.state = state;
-            new_node.action = "left";
-            new_node.path_cost = node.path_cost;
-        }
+        // update new node
+        new_node.parent = &node;
+        new_node.state = state;
+        new_node.action = "left";
+        new_node.act_path_cost = node.act_path_cost + 1;
+//        new_node.path_cost = manhattan_heuristic(state);
     }
     else if(move == MOVE::UP && (blank_pos != 0 && blank_pos != 1 && blank_pos != 2)){
         auto neighbor_tile = state[blank_pos - 3];
         state[blank_pos - 3] = 'b';
         state[blank_pos] = neighbor_tile;
 
-        try{
-            // check if new state is reversal of previous move
-            tree.get_explored_set()->at(state);
-            // node has already been generated, fall through to return statement
-            //new_node.parent = node.parent;// debugging: fix to avoid creating new node
-        }
-        catch(const std::out_of_range& oor){
-            // new_node hasn't been generated before, update new node
-            new_node.parent = &node;
-            new_node.state = state;
-            new_node.action = "up";
-            new_node.path_cost = node.path_cost;
-        }
+        // update new node
+        new_node.parent = &node;
+        new_node.state = state;
+        new_node.action = "up";
+        new_node.act_path_cost = node.act_path_cost + 1;
+  //      new_node.path_cost = manhattan_heuristic(state);
     }
     else if(move == MOVE::RIGHT && (blank_pos != 2 && blank_pos != 5 && blank_pos != 8)){
         auto neighbor_tile = state[blank_pos + 1];
         state[blank_pos + 1] = 'b';
         state[blank_pos] = neighbor_tile;
 
-        try{
-            // check if new state is reversal of previous move
-            tree.get_explored_set()->at(state);
-            // node has already been generated, fall through to return statement
-            //new_node.parent = node.parent;// debugging: fix to avoid creating new node
-        }
-        catch(const std::out_of_range& oor){
-            // new_node hasn't been generated before, update new node
-            new_node.parent = &node;
-            new_node.state = state;
-            new_node.action = "right";
-            new_node.path_cost = node.path_cost;
-        }
+        // update new node
+        new_node.parent = &node;
+        new_node.state = state;
+        new_node.action = "right";
+        new_node.act_path_cost = node.act_path_cost + 1;
+    //    new_node.path_cost = manhattan_heuristic(state);
     }
     else if(move == MOVE::DOWN && (blank_pos != 6 && blank_pos != 7 && blank_pos != 8)){
         auto neighbor_tile = state[blank_pos + 3];
         state[blank_pos + 3] = 'b';
         state[blank_pos] = neighbor_tile;
 
-        try{
-            // check if new state is reversal of previous move
-            tree.get_explored_set()->at(state);
-            // node has already been generated, fall through to return statement
-            //new_node.parent = node.parent;// debugging: fix to avoid creating new node
-        }
-        catch(const std::out_of_range& oor){
-            // new_node hasn't been generated before, update new node
-            new_node.parent = &node;
-            new_node.state = state;
-            new_node.action = "down";
-            new_node.path_cost = node.path_cost;
-        }
+        // update new node
+        new_node.parent = &node;
+        new_node.state = state;
+        new_node.action = "down";
+        new_node.act_path_cost = node.act_path_cost;
+      //  new_node.path_cost = manhattan_heuristic(state);
     }
-    return node;
+    return new_node;
 }
 
-bool goal_test(std::string goal_state, std::string test_state)
+bool EightPuzzle::goal_test(std::string goal_state, std::string test_state)
 {
     return goal_state == test_state;
 }
+
+std::vector<Node> EightPuzzle::generate_successor(Node &parent, heuristic_type funct_pnt)
+{
+    std::vector<Node> successor;
+    for(int i = 0; i != 4; ++i){// 4 is because there are four moves to generate a child: left, up, right, down
+        // add code to skip checking action equal to current node action
+        auto new_child = move(parent, static_cast<MOVE>(i));
+        if(new_child.action != "none"){
+            new_child.path_cost = (this->*funct_pnt)(new_child.state);
+            successor.push_back(new_child);
+        }
+    }
+    return successor;
+}
+
+void EightPuzzle::set_goal_state(std::string goal_arg)
+{
+    goal_state = goal_arg;
+}
+
+std::string EightPuzzle::get_goal_state()
+{
+    return goal_state;
+}
+
+void EightPuzzle::print_node(Node &node)
+{
+    std::cout << "\nparent pointer: " << node.parent
+    << "\naction: " << node.action
+    << "\nstate: " << node.state
+    << "\npath_cost: " << node.path_cost
+    << "\nact_path_cost: " << node.act_path_cost << std::endl;
+}
+
+/*void EightPuzzle::add_node(const std::string &key_state, const Node &val_node)
+{
+    std::string state(key_state);
+    Node node(val_node);
+    //std::pair<std::string, Node> pair = std::make_pair<std::string, Node>(std::string(state), Node(node));
+    tree.get_all_nodes()->insert(std::make_pair<std::string, Node>(std::string(state), Node(node)));
+}
+*/
 /*
 double EightPuzzle::search_time()
 {
@@ -114,38 +131,44 @@ double EightPuzzle::search_time()
     return 0;
 }
 */
-unsigned EightPuzzle::manhattan_heuristic(const Node &node)
+unsigned EightPuzzle::manhattan_heuristic(const std::string &node_state)
 {
-    // the sum of the horizontal and vertical distances of the tiles from their goal positions
-    // will return the h(n) portion of f(n)
+    std::cout << "in manhattan!" << std::endl;//debug
     unsigned ver_dis(0);
     unsigned hor_dis(0);
 
-    // pos / 3 will give row, pos % 3 will give col
+    // (pos / 3) will give row, (pos % 3) will give col
     for(std::size_t pos = 0; pos != goal_state.size(); ++pos){
-        if(goal_state[pos] != node.state[pos]){
-            // find col and row of goal state
-            auto goal_row = pos / 3;
-            auto goal_col = pos % 3;
+        if(goal_state[pos] != node_state[pos]){
 
             // find col and row of node state
-            std::size_t node_pos = node.state.find(goal_state[pos]);
-            auto node_row = node_pos / 3;
-            auto node_col = node_pos % 3;
+            auto node_row(0);
+            auto node_col(0);
+            if(pos < 3){
+                node_row = pos / 3;
+                node_col = pos;
+            }
+            else{
+                node_row = pos / 3;
+                node_col = pos % 3;
+            }
+            // find col and row of goal state
+            std::size_t goal_pos = goal_state.find(node_state[pos]);
+            auto goal_row = goal_pos / 3;
+            auto goal_col = goal_pos % 3;
 
-            ver_dis += std::abs(static_cast<double>(goal_row - node_row));
-            hor_dis += std::abs(static_cast<double>(goal_col - node_col));
+            ver_dis += std::abs(static_cast<int>(goal_row - node_row));
+            hor_dis += std::abs(static_cast<int>(goal_col - node_col));
         }
     }
     return ver_dis + hor_dis;
 }
 
-unsigned EightPuzzle::misplaced_tile_heuristic(const Node &node)
+unsigned EightPuzzle::misplaced_tile_heuristic(const std::string &node_state)
 {
-    // the number of misplaced tiles
     unsigned misplaced_tile(0);
     for(std::size_t i = 0; i != goal_state.size(); ++i){
-        if(goal_state[i] != node.state[i]){
+        if(goal_state[i] != node_state[i]){
             ++misplaced_tile;
         }
     }
@@ -153,30 +176,42 @@ unsigned EightPuzzle::misplaced_tile_heuristic(const Node &node)
 }
 
 // NOTE: need to generate actual g(n) not use from heuristic
-void EightPuzzle::a_star_search(const std::string &initial_state, Node &result)
+void EightPuzzle::a_star_search(const std::string &initial_state, Node &result, heuristic_type funct_pnt)
 {
-    // give root initial state and add to frontier_set
+    std::cout << "Starting A* search" << std::endl;//debug
+    // give root initial state and add to tree
     Node root;
     root.state = initial_state;
-    tree.get_frontier_set()->push(root);
+    tree.get_all_nodes()->insert(root);
+    tree.get_frontier_set()->push(&root);
 
+    std::cout << "Completed root node setup" << std::endl;//debug
     // get top node and start searching for goal
-    Node current = Node(tree.get_frontier_set()->top());
-    //while(!tree.get_frontier_set()->empty() && /*(current = &Node(tree.get_frontier_set()->top()))) && */goal_test(goal_state, current.state)){
-    while(!tree.get_frontier_set()->empty() && goal_test(goal_state, current.state)){
+    Node current = *(tree.get_frontier_set()->top());
+    auto cnt(0);// debug
+    while(++cnt <= 10 && !tree.get_frontier_set()->empty() && !goal_test(goal_state, current.state)){
+        /*
         if(current.state == initial_state){
+    std::cout << "in intial state block" << std::endl;//debug
             continue; // don't process root node
         }
+        */
         //Node current_node = tree.get_frontier_set()->top();
-        tree.get_frontier_set()->pop();
 
         // if the node isn't the goal generate it's successors and check 3 cases:
-        for(int i = 0; i != 4; ++i)// 4 is because there are four moves to generate a child: left, up, right, down
+        //for(int i = 0; i != 4; ++i){// 4 is because there are four moves to generate a child: left, up, right, down
+            // update for loop to generat_successor function call
             // add code to skip checking action equal to current node action
-            auto child = move(current, static_cast<MOVE>(i));
-            Node former_child;
+            //auto child = move(current, static_cast<MOVE>(i));//remove
+
+    std::cout << "Generating successors" << std::endl;//debug
+        auto successors = generate_successor(current, funct_pnt);
+        for(auto child : successors){
+    std::cout << "searching for former child" << std::endl;//debug
+            Node *former_child = nullptr;
             bool found(false);
 
+/*
             std::iterator<std::priority_queue<Node, std::vector<Node>, std::greater<Node>>> it;
             for(auto ele : tree.get_frontier_set()){
                 if(ele.state == child.state){
@@ -185,39 +220,153 @@ void EightPuzzle::a_star_search(const std::string &initial_state, Node &result)
                     break;
                 }
             }
-        if(found && former_child.path_cost > child.path_cost){
-        // child is in frontier_set with greater path_cost, update child
-            former_child = child;
-        }
-        else{
-            try{
-                auto former_child = get_tree()->get_explored_set()->at(state);
-
-                // child is in the explored_set with greater path_cost
-                // update child and move to frontier_set
-                if(former_child.path_cost > child.path_cost){
-                    former_child = child;
+*/
+            for(auto ele : *(tree.get_all_nodes())){
+                if(ele.state == child.state){
+    std::cout << "former child found" << std::endl;//debug
+                    found = true;
+                    former_child = &ele;
+                    break;
                 }
             }
-            catch(const std::out_of_range& oor){
+            if(!found){// && former_child.path_cost > child.path_cost){
                 // child is in neither frontier_set nor explored_set
-                // add child to frontier
-                tree.get_frontier_set()->push(child);
+                // add child to tree
+    std::cout << "adding new child to tree" << std::endl;//debug
+                tree.get_all_nodes()->insert(child);
+                tree.get_frontier_set()->push(&child);
+                //print_node(child);//debug
+            }
+            else{
+                try{
+                    /*auto former_child = */get_tree()->get_explored_set()->at(child.state);
+
+                    // child is in the explored_set with greater path_cost
+                    // update child and move to frontier_set
+                    if(former_child->path_cost > child.path_cost){
+    std::cout << "moving former child from explored set to frontier set" << std::endl;//debug
+                //print_node(child);//debug
+                        *former_child = child;
+                        tree.get_explored_set()->erase(former_child->state);
+                        tree.get_frontier_set()->push(former_child);
+                    }
+                }
+                catch(const std::out_of_range& oor){
+                // child is in frontier_set with greater path_cost, update child
+                    if(former_child->path_cost > child.path_cost){
+    std::cout << "former child is in frontier, update" << std::endl;//debug
+                        *former_child = child;
+                    }
+                }
             }
         }
         // add current node to explored_set
-        explored_set[current.state] = current;
+        //tree.get_all_nodes()->insert(cu
+    std::cout << "current node state: " << current.state << std::endl; //debug
+        (*tree.get_explored_set())[current.state] = tree.get_frontier_set()->top();
+        tree.get_frontier_set()->pop();
+        current = *(tree.get_frontier_set()->top());
+        print_node(current);//debug
+        ++num_nodes_expanded;
+    std::cout << "at the end of while loop" << std::endl;//debug
     }
-    result = tree.get_frontier_set()->top();
+    result = *(tree.get_frontier_set()->top());
+    std::cout << "Ending A* search" << std::endl;//debug
 }
 
-void EightPuzzle::ida_search(const std::string &initial_state, Node &result)
+void EightPuzzle::a_star_search_manhattan(const std::string &initial_state, Node &result)
 {
+    a_star_search(initial_state, result, man_func);
+}
+
+void EightPuzzle::a_star_search_misplaced_tile(const std::string &initial_state, Node &result)
+{
+    a_star_search(initial_state, result, mis_tile_func);
+}
+
+
+void EightPuzzle::ida_search(const std::string &initial_state, Node &result, heuristic_type funct_pnt)
+{
+    // create root node and add to tree
     Node root;
-    //root.state;
+    root.state = initial_state;
+    tree.get_all_nodes()->insert(root);
+    tree.get_frontier_set()->push(&root);
+
+    // set score of best solution L, to max
+    decltype(root.path_cost) min_f(UINT_MAX);
+    unsigned L = min_f;
+
+    // loop until frontier_set is empty
+    Node current = *(tree.get_frontier_set()->top());
+
+    while(!tree.get_frontier_set()->empty()){
+
+        // get top element from frontier_set
+        // check if element is the goal
+        if(goal_test(goal_state, current.state)){
+            // yes => L = min(element.path_cost, L)
+            L = L <= current.path_cost ? L : current.path_cost;
+        }
+        else{
+            // no => generate sucessors
+            auto successor = generate_successor(current, funct_pnt);
+
+            for(auto &child : successor){
+                if(child.path_cost >= L){
+                    continue;
+                }
+                else{
+                    // sucessor.path_cost < L, add to tree and frontier
+                    tree.get_all_nodes()->insert(child);
+                    tree.get_frontier_set()->push(&child);
+                }
+                if(child.path_cost > L && child.path_cost < min_f){
+                    min_f = child.path_cost;
+                }
+            }
+        }
+        L = min_f;// update L to next contour
+    }
 }
 
-void EightPuzzle::df_branch_bound_search(const std::string &initial_state, Node &result)
+void EightPuzzle::df_branch_bound_search(const std::string &initial_state, Node &result, heuristic_type funct_pnt)
 {
+    // create root node and add to tree
+    Node root;
+    root.state = initial_state;
+    tree.get_all_nodes()->insert(root);
+    tree.get_frontier_set()->push(&root);
 
+    // set score of best solution L, to max
+    unsigned L = UINT_MAX;
+
+    // loop until frontier_set is empty
+    Node current = *(tree.get_frontier_set()->top());
+
+    while(!tree.get_frontier_set()->empty()){
+
+        // get top element from frontier_set
+        // check if element is the goal
+        if(goal_test(goal_state, current.state)){
+            // yes => L = min(element.path_cost, L)
+            L = L <= current.path_cost ? L : current.path_cost;
+        }
+        else{
+            // no => generate sucessors
+            auto successor = generate_successor(current, funct_pnt);
+
+            for(auto &child : successor){
+                if(child.path_cost >= L){
+                    continue;
+                }
+                else{
+                    // sucessor.path_cost < L, add to tree and frontier
+                    tree.get_all_nodes()->insert(child);
+                    tree.get_frontier_set()->push(&child);
+                }
+            }
+        }
+    }
 }
+
--- CPP_Primer/AI_F17/8_Puzzle/Search_Tree/Node.h
+++ CPP_Primer/AI_F17/8_Puzzle/Search_Tree/Node.h
@@ -1,4 +1,4 @@
-//AUTHOR: AU
+//AUTHOR: NA
 //DATE: 09/18/17
 //FILE: Node.h
 //DESC: Define all 8-puzzle search elements
@@ -8,6 +8,7 @@
 
 #include <queue>
 #include <vector>
+#include <set>
 #include <unordered_map>
 #include <string>
 #include <climits>
@@ -19,68 +20,108 @@ struct Node{
 
     Node(Node *parent_arg, std::string state_arg, std::string action_arg, int path_cost_arg) : parent(parent_arg), state(state_arg), action(action_arg), path_cost(path_cost_arg){ }
 
-    bool operator>(const Node& rhs) const
+    // overloaded for set container
+    bool operator<(const Node &rhs) const
     {
-        return this->path_cost > rhs.path_cost;
+        return this->path_cost < rhs.path_cost;
     }
 
     // DATA MEMBERS //
     Node *parent = nullptr;
     std::string state;
     std::string action = "none";
-    unsigned path_cost = UINT_MAX;
+    unsigned path_cost = UINT_MAX;// may change to g(n) and h(n)
+    unsigned act_path_cost = 0;
 };
 
+// represent 8-puzzle actions
 enum class MOVE{
-        LEFT = 0,
-        UP,
-        RIGHT,
-        DOWN,
+    LEFT = 0,
+    UP,
+    RIGHT,
+    DOWN,
 };
 
+// Custom compare function for priority_queue
+struct CompareNode{
+    bool operator()(const Node *lhs, const Node *rhs) const{
+        return (lhs->path_cost + lhs->act_path_cost) > (rhs->path_cost + rhs->path_cost);
+    }
+};
+
+// Collection of containers to hold Nodes
 class SearchTree{
 
     public:
 
     // TYPE MEMBERS //
-    using queue_type = std::priority_queue<Node, std::vector<Node>, std::greater<Node>>;
-    using hash_type = std::unordered_map<std::string, Node>;
+    using set_type = std::set<Node>;
+    using queue_type = std::priority_queue<Node*, std::vector<Node*>, CompareNode>;
+    using hash_type = std::unordered_map<std::string, Node*>;
 
     // MEMBER FUNCTIONS //
     SearchTree() = default;
+    set_type* get_all_nodes() { return &all_nodes; }
     queue_type* get_frontier_set() { return &frontier_set; }
     hash_type* get_explored_set() { return &explored_set; }
 
     private:
 
     // DATA MEMBERS //
+    set_type all_nodes;
     queue_type frontier_set;
     hash_type explored_set;
 };
 
+// Plays the game
 class EightPuzzle {
+
     public:
+
+    // TYPE MEMBERS //
+    using heuristic_type = unsigned (EightPuzzle::*)(const std::string &node_state);
 
     // MEMBER FUNCTIONS //
     EightPuzzle() = default;
-    EightPuzzle(std::string goal_state_arg) : goal_state(goal_state_arg) { }
+    EightPuzzle(std::string goal_state_arg) : goal_state(goal_state_arg) 
+    {
+        man_func = &EightPuzzle::manhattan_heuristic;
+        mis_tile_func = &EightPuzzle::misplaced_tile_heuristic;
+    }
+
+    // helper functions
     SearchTree* get_tree() { return &tree; }
-    Node move(/*const*/ Node &node, MOVE move/*const std::string &move*/);
+    Node move(Node &node, MOVE move);
     bool goal_test(std::string goal_state, std::string test_state);
-    //double search_time();// use <ctime> clock function
-    unsigned manhattan_heuristic(const Node &node);
-    unsigned misplaced_tile_heuristic(const Node &node);
-    void a_star_search(const std::string &initial_state, Node &result);
-    void ida_search(const std::string &initial_state, Node &result);
-    void df_branch_bound_search(const std::string &initial_state, Node &result);
+    std::vector<Node> generate_successor(Node &parent, heuristic_type funct_pnt);
+    void set_goal_state(std::string goal_arg);
+    std::string get_goal_state();
+    void print_node(Node &node);
+    //void add_node(Node &node);
+
+    // heuristic functions
+
+    // the sum of the horizontal and vertical distances of the tiles from their goal positions. Will return the h(n) portion of f(n)
+    unsigned manhattan_heuristic(const std::string &node_state);
+    // the number of misplaced tiles
+    unsigned misplaced_tile_heuristic(const std::string &node_state);
+
+    // search algorithms
+    void a_star_search(const std::string &initial_state, Node &result, heuristic_type funct_pnt);
+    void a_star_search_manhattan(const std::string &initial_state, Node &result);
+    void a_star_search_misplaced_tile(const std::string &initial_state, Node &result);
+    void ida_search(const std::string &initial_state, Node &result, heuristic_type funct_pnt);
+    void df_branch_bound_search(const std::string &initial_state, Node &result, heuristic_type funct_pnt);
 
     private:
+
+    // DATA MEMBERS //
     SearchTree tree;
     std::string goal_state;
+    unsigned num_nodes_expanded = 0;
     std::vector<int> heuristic_table;
-    std::vector<Node> search_result;
-    std::vector<clock_t> search_time;
-    
-    // DATA MEMBERS //
+    std::vector<std::pair<Node, clock_t> > search_result; // will hold goal state with search time for each algorithm
+    heuristic_type man_func;
+    heuristic_type mis_tile_func;
 };
 #endif
